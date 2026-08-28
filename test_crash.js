9068a423e6f792804561fa04cf65f27327272bf6
if-else-repeat <prm4672@gmail.com>
2026-08-28 16:33:56 +0530

    fix vercel routing and clean repo

--- test_crash.js
+++
@@ -1,6 +0,0 @@
-const http = require('http');
-http.get('http://localhost:5173/', (res) => {
-  let data = '';
-  res.on('data', (chunk) => data += chunk);
-  res.on('end', () => console.log('Response size:', data.length, 'bytes'));
-});
--- test_react.js
+++
@@ -1,7 +0,0 @@
-import jsdom from 'jsdom';
-const { JSDOM } = jsdom;
-JSDOM.fromURL("http://localhost:3000/prototype/epfo", { runScripts: "dangerously", resources: "usable" }).then(dom => {
-  setTimeout(() => {
-    console.log(dom.window.document.body.innerHTML);
-  }, 3000);
-});
